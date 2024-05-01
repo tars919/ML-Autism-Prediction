ae8f58e0dc4269fc0056edbd5b4aa0e3ce184161
tars919 <138830040+tars919@users.noreply.github.com>
2024-05-01 12:52:29 -0400

    Update autismpred.py
    understood the imported data and cleaned the data

--- autismpred.py
+++ autismpred.py
@@ -19,9 +19,44 @@ from imblearn.over_sampling import RandomOverSampler
 import warnings
 warnings.filterwarnings('ignore')
 
+#commenting out the print statements when needed
 #loading the dataset into the pandas data frame 
-#then printing its first 5 rows
 df = pd.read_csv('train.csv')
-print(df.head())
+
+#then printing its first 5 rows
+#print(df.head())
+
+#checking the size of the dataset
+#print(df.shape)
+
+#to check which column of the dataset contains which type of data
+#print(df.info())
+
+#Shows us in each column that there are no null values
+#print(df.describe().T)
+
+
+
+#- - - - - - - - - -- - - - - - - - - - - - - -- - - - - - - - -- - -- - - - - 
+#DATA CLEANING: This step will remove ouliers, null value imputation, and removing discrepancies
+
+#displays all the ethnicities and how many of each ethnicity
+#print(df['ethnicity'].value_counts())
+
+#this is counting the different relations and how many of each -> shows us theres a ? and others when there should just be one of that catagory
+#print(df['relation'].value_counts())
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Cleaning the dats ~~~ and converting yes to 0 and no to 1
+#groups ? and others into the same Others category
+df = df.replace({'yes':1, 'no':0, '?':'Others', 'others':'Others'})
+
+#- - - - - - - - - -- - - - - - - - - - - - - -- - - - - - - - -- - -- - - - - 
+#EXPLORATORY DATA ANALYSIS
+
+
+
+
+
 
 
